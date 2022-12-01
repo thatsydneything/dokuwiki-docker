1d4f096c12132e78e55bcc0f3f173465ff15f244
Lachlan Stevens <stevens.lachlan@gmail.com>
2022-12-01 21:31:25 +1100

    Simplify entrypoint and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM debian:bullseye-slim
+FROM ubuntu:22.10
 
-LABEL maintainer="Lachlan Stevens"
-LABEL org.opencontainers.image.source=https://github.com/thatsydneything
+LABEL maintainer="That Sydney Thing"
+LABEL org.opencontainers.image.source=https://github.com/thatsydneything/dokuwiki-docker
 
 ENV DEBIAN_FRONTEND noninteractive
 ENV LANG EN
@@ -9,12 +9,13 @@ ENV ACL ON
 ENV ACL_POLICY 2
 ENV LICENSE 0
 ENV ALLOW_REG OFF
-ENV POP ON
+ENV POP OFF
 ENV TITLE DokuWiki 
 ENV USERNAME WikiUser
-ENV PASSWORD Password1
-ENV FULL_NAME "Test WikiUser"
+ENV PASSWORD ""
+ENV FULL_NAME "Wiki User"
 ENV EMAIL test@test.com
+ENV DEBUG_DISABLE_INSTALL 0
 
 RUN apt update && apt install -y php \ 
                 libapache2-mod-php \
@@ -29,19 +30,22 @@ RUN apt update && apt install -y php \
                 && apt clean \
                 && rm -rf /var/lib/apt/lists /var/cache/apt/archives
 
-RUN wget https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz && tar xzvf dokuwiki-stable.tgz && mv dokuwiki-*a /usr/share/dokuwiki && mv /usr/share/dokuwiki/lib /tmp && mv /usr/share/dokuwiki/conf /tmp && mv /usr/share/dokuwiki/data /tmp && mkdir /usr/share/dokuwiki/lib && mkdir /usr/share/dokuwiki/conf && mkdir /usr/share/dokuwiki/data
-
-RUN chown -R www-data:www-data /usr/share/dokuwiki
-
-RUN echo 'Listen 8080' >> /etc/apache2/apache2.conf
+RUN wget https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz && tar xzvf dokuwiki-stable.tgz \
+    && mv dokuwiki-*a /usr/share/dokuwiki && mv /usr/share/dokuwiki/lib /tmp && mv /usr/share/dokuwiki/conf /tmp \
+    && mv /usr/share/dokuwiki/data /tmp && mkdir /usr/share/dokuwiki/lib \
+    && mkdir /usr/share/dokuwiki/conf && mkdir /usr/share/dokuwiki/data && chown -R www-data:www-data /usr/share/dokuwiki/* && chown -R www-data:www-data /tmp \
+    && chown -R www-data:www-data /var/log/apache2 && chown -R www-data:www-data /var/run/apache2 && chown -R www-data:www-data /etc/apache2 && chown -R www-data:www-data /var/lib/apache2 \
+    && sed -i 's/Listen 80/Listen 8080/' /etc/apache2/ports.conf && sed -i 's/Listen 443/Listen 8443/' /etc/apache2/ports.conf \
+    && sed -i 's/Listen 80/Listen 8080/' /etc/apache2/sites-enabled/000-default.conf && sed -i 's/Listen 443/Listen 8443/' /etc/apache2/sites-enabled/000-default.conf
 
 COPY entrypoint.sh /
 COPY apache.conf /etc/dokuwiki/apache.conf
 
-RUN ln -s /etc/dokuwiki/apache.conf /etc/apache2/sites-available/dokuwiki.conf
-
-RUN ["chmod", "+x", "/entrypoint.sh"]
+RUN ln -s /etc/dokuwiki/apache.conf /etc/apache2/sites-available/dokuwiki.conf \
+    && chmod +x /entrypoint.sh && chmod g+rwX /usr/share/dokuwiki
 
 EXPOSE 8080 8443
+
+USER www-data
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,25 +11,6 @@ echo '#############################'
 echo 'For any issues or assistance, check us out on GitHub:'
 echo 'http://github.com/thatsydneything/dokuwiki-docker/'
 
-# echo 'Waiting for Apache to be ready...'
-
-# IP_ADDRESS=$(hostname -I)
-
-# APACHE_ATTEMPTS=0
-# APACHE_RESPONSE=0
-
-# while [ $APACHE_RESPONSE -ne 200 ]
-# do
-#     if [ $APACHE_ATTEMPTS -ge 10 ]
-#     then
-#         echo $APACHE_ATTEMPTS
-#         echo 'Apache service is not correctly configured. Terminating...'
-#         exit 1
-#     fi
-#     sleep 5
-#     APACHE_RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" http://${IP_ADDRESS}:8080/doku.php)
-# done
-
 echo 'Checking for Dokuwiki persisted data...'
 
 SETUP_REQUIRED=false
@@ -38,6 +19,7 @@ if [ -z "$(ls -A /usr/share/dokuwiki/data/)" ]
 then
   echo 'No existing Dokuwiki data folder detected... copying new data...'
   cp -R /tmp/data/. /usr/share/dokuwiki/data/
+  chown -R www-data:www-data /usr/share/dokuwiki/data/
 else
   echo 'Existing Dokuwiki data folder detected...'
 fi
@@ -46,6 +28,7 @@ if [ -z "$(ls -A /usr/share/dokuwiki/lib/)" ]
 then
   echo 'No existing Dokuwiki library folder detected... copying new data...'
   cp -R /tmp/lib/. /usr/share/dokuwiki/lib/
+  chown -R www-data:www-data /usr/share/dokuwiki/lib/
 else
   echo 'Existing Dokuwiki library folder detected...'
 fi
@@ -54,12 +37,11 @@ if [ -z "$(ls -A /usr/share/dokuwiki/conf/)" ]
 then
   echo 'No existing Dokuwiki configuration folder detected... copying new data...'
   cp -R /tmp/conf/. /usr/share/dokuwiki/conf/
+  chown -R www-data:www-data /usr/share/dokuwiki/conf/
   SETUP_REQUIRED=true
 else
   echo 'Existing Dokuwiki configuration folder detected...'
 fi
-
-chown -R www-data:www-data /usr/share/dokuwiki
 
 echo 'Finished loading Dokuwiki persistence data...'
 
@@ -70,48 +52,22 @@ a2ensite dokuwiki
 service apache2 reload
 
 echo 'Apache service started...'
-
 if [ ${SETUP_REQUIRED} == true ]
 then
   echo 'Configuring Dokuwiki installation from environment variables...'
   echo 'Validating environment variables...'
 
-  VAR_ERROR=false
-
-  if [ -z ${USERNAME} ]
-  then
-    echo 'ERROR: USERNAME not defined in environment variables.'
-    VAR_ERROR=true
-  fi
-
   if [ -z ${PASSWORD} ]
   then
-    echo 'ERROR: PASSWORD not defined in environment variables.'
-    VAR_ERROR=true
-  fi
-
-  if [ -z ${FULL_NAME} ]
-  then
-    echo 'ERROR: FULL_NAME not defined in environment variables.'
-    VAR_ERROR=true
-  fi
-
-  if [ -z ${EMAIL} ]
-  then
-    echo 'ERROR: EMAIL not defined in environment variables.'
-    VAR_ERROR=true
-  fi
-
-  if [ ${VAR_ERROR} == true ]
-  then
-    echo 'Please provide required environment variables specified above and start pod again. Terminating...'
+    echo 'ERROR: PASSWORD not defined in environment variables. Terminating...'
     rm -rf /usr/share/dokuwiki/conf/*
     exit 1
   else
     echo 'Successfully validated environment variables...'
     echo 'Attempting to run install script...'
-    CURL_RESPONSE="$(curl  --verbose --location --data-urlencode l="${LANG}" --data-urlencode d[acl]="${ACL}" --data-urlencode d[policy]="${ACL_POLICY}" --data-urlencode d[allowreg]="${ALLOW_REG}" --data-urlencode d[license]="${LICENSE}" --data-urlencode d[pop]="${POP}" --data-urlencode d[title]="${TITLE}" --data-urlencode d[superuser]="${USERNAME}" --data-urlencode d[fullname]="${FULL_NAME}" --data-urlencode d[email]="${EMAIL}" --data-urlencode d[password]="${PASSWORD}" --data-urlencode d[confirm]="${PASSWORD}" --data-urlencode submit= http://127.0.0.1:8080/install.php)"
-    if [ ${CURL_RESPONSE} == *"The configuration was finished successfully"* ]
+    sleep 1
+    CURL_RESPONSE="$(curl  --verbose --location --data-urlencode l="${LANG}" --data-urlencode d[acl]="${ACL}" --data-urlencode d[policy]="${ACL_POLICY}" --data-urlencode d[allowreg]="${ALLOW_REG}" --data-urlencode d[license]="${LICENSE}" --data-urlencode d[pop]="${POP}" --data-urlencode d[title]="${TITLE}" --data-urlencode d[superuser]="${USERNAME}" --data-urlencode d[fullname]="${FULL_NAME}" --data-urlencode d[email]="${EMAIL}" --data-urlencode d[password]="${PASSWORD}" --data-urlencode d[confirm]="${PASSWORD}" --data-urlencode submit= http://127.0.0.1:8080/install.php 2>&1)"
+    if [[ "${CURL_RESPONSE}" == *"The configuration was finished successfully"* ]]
     then  
       echo 'Dokuwiki installation completed successfully...'
     else
